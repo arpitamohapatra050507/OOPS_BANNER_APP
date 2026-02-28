230b0fce49dc0c87f72e40f4052490bf0ffc013b
Arpita <Arpitamohapatra050507@gmail.com>
2026-03-01 02:08:35 +0530

    "Updated UC4 logic"

--- UC4.java
+++ UC4.java
@@ -1,6 +1,6 @@
 public class UC4 {
      public static void main(String[] args) {
-        // UC4: Store lines in a String Array to eliminate repetitive code
+       
         String[] banner = new String[7];
 
         banner[0] = String.join("","  ***** " , "   ***** " , "  ***** " , "  ***** ");
@@ -9,7 +9,7 @@ public class UC4 {
         banner[3] = String.join(""," *     * "  ," *     * " , " ***** " , "  ***** ");
         banner[4] = String.join(""," *     * " , " *     * " , " * " , "          * ");
         banner[5] = String.join(""," *     * " , " *     * " , " * " , "          * ");
-        banner[6] = String.join("","  ***** " , "   *****  "  , " * " , "      ***** ");
+        banner[6] = String.join("","  ***** " , "   *****  "  , "* " , "      ***** ");
         for (String line : banner) {
             System.out.println(line);
         }
